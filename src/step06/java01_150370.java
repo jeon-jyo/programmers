6650f173d5e628223289e606317d25edaa9297d6
jyo <qwer790488@gmail.com>
2024-03-16 19:40:31 +0900

    update

--- src/step06/java01_150370.java
+++ src/step06/java01_150370.java
@@ -7,14 +7,15 @@ import java.util.*;
 public class java01_150370 {
     public static void main(String[] args) {
         String today = "2020.01.01";
-        String[] terms = {"Z 3", "D 5"};
-        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+        String[] terms = {"Z 3", "D 6"};
+        String[] privacies = {"2019.06.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
 
         System.out.println(Arrays.toString(solution(today, terms, privacies)));
     }
 
     public static int[] solution(String today, String[] terms, String[] privacies) {
-        int[] todayList = new int[] {Integer.parseInt(today.split("\\.")[0]), Integer.parseInt(today.split("\\.")[1]), Integer.parseInt(today.split("\\.")[2])};
+        String[] str = new String[] {today.split("\\.")[0], today.split("\\.")[1], today.split("\\.")[2]};
+        int[] todayList = new int[] {Integer.parseInt(str[0]), Integer.parseInt(str[1]), Integer.parseInt(str[2])};
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < privacies.length; i++) {
@@ -27,8 +28,8 @@ public class java01_150370 {
 
                 if (privacy.equals(kind)) {
                     int term = Integer.parseInt(terms[j].split(" ")[1]);
-                    int year = date[0]+(date[1]+term)/12;
-                    int month = (date[1]+term)%12;
+                    int year = date[0]+(date[1]+term-1)/12;
+                    int month = (date[1]+term-1)%12+1;
 
                     if (year < todayList[0]) {
                         list.add(i+1);
